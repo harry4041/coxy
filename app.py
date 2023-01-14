579013778a2b5e27fca622c139cabed327fe3afd
Harry Cox <hrjcox@outlook.com>
2023-01-14 20:23:32 +0000

    Starting to look like a site

--- app.py
+++ app.py
@@ -2,5 +2,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('home.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
